e00e637086a0524dd9debc8928da796c7db84ed1
Andrew Epstein <aepstein@udel.edu>
2016-09-19 17:21:49 -0400

    calculate best hand based on given 2 cards

--- card.hpp
+++ card.hpp
@@ -21,13 +21,18 @@
 #include <ostream>
 #include <iostream>
 
+//#define DEBUG_OUT(x) cout << x << endl
+#define DEBUG_OUT(x)
+
 using namespace std;
 
 enum class Suit : int {
 	spades,
 	clubs,
 	diamonds,
-	hearts
+	hearts,
+	First = spades,
+	Last = hearts
 };
 
 enum class Rank : int {
@@ -71,7 +76,18 @@ public:
 };
 
 ostream &operator<<( ostream &os, const Card &card ) {
-	os << static_cast<int>( card.getRank() ) << static_cast<int>( card.getSuit() );
+	static vector<string> suitMap = { "♠", "♣", "♦", "♥" };
+	static vector<string> rankMap = { "Jack", "Queen", "King", "Ace" };
+	int rank = static_cast<int>( card.getRank() ) + 2;
+	int suit = static_cast<int>( card.getSuit() );
+
+	if( rank > 10 ) {
+		os << rankMap[rank - 11];
+	} else {
+		os << rank;
+	}
+
+	os << suitMap[suit];
 	return os;
 }
 
@@ -120,11 +136,23 @@ vector<int> rankCounts( vector<Card> cards ) {
 	return counts;
 }
 
-int hasStraight( vector<int> counts ) {
+bool hasStraight( vector<int> counts ) {
 	for( int i = 0; i <= 8; ++i ) {
 		if( counts[i] == 1 && counts[i + 1] == 1 && counts[i + 2] == 1 && counts[i + 3] == 1 && counts[i + 4] == 1 ) {
 			return true;
 		}
+	}
+
+	return false;
+}
+
+bool hasRoyalFlush( vector<Card> cards, vector<int> counts ) {
+	if( !hasFlush( cards ) ) {
+		return false;
+	}
+
+	if( counts[8] == 1 && counts[9] == 1 && counts[10] == 1 && counts[11] == 1 && counts[12] == 1 ) {
+		return true;
 	}
 
 	return false;
@@ -148,4 +176,52 @@ int getBaseScore( vector<int> ranks ) {
 	}
 
 	return result;
+}
+
+int score( vector<Card> hand ) {
+	int result = 0;
+	auto counts = rankCounts( hand );
+
+	// Count the number of pairs, three of a kind, etc. E.g. if there are two pairs, countsHist[2] == 2.
+	vector<int> countsHist( 5, 0 );
+
+	for( auto && count : counts ) {
+		countsHist[count]++;
+	}
+
+	int baseScore = getBaseScore( counts );
+	bool flush = hasFlush( hand );
+	bool straight = hasStraight( counts );
+
+	// royal flush
+	if( hasRoyalFlush( hand, counts ) ) {
+		DEBUG_OUT( "Royal Flush" );
+		result = 9;
+	} else if( straight && flush ) { // straight flush
+		DEBUG_OUT( "Straight Flush" );
+		result = 8;
+	} else if( countsHist[4] == 1 ) { // four of a kind
+		DEBUG_OUT( "Four of a Kind" );
+		result = 7;
+	} else if( countsHist[3] == 1 && countsHist[2] == 1 ) { // full house
+		DEBUG_OUT( "Full House" );
+		result = 6;
+	} else if( flush ) { // flush
+		DEBUG_OUT( "Flush" );
+		result = 5;
+	} else if( straight ) { // straight
+		DEBUG_OUT( "Straight" );
+		result = 4;
+	} else if( countsHist[3] == 1 ) { // three of a kind
+		DEBUG_OUT( "Three of a Kind" );
+		result = 3;
+	} else if( countsHist[2] == 2 ) { // two pairs
+		DEBUG_OUT( "Two Pairs" );
+		result = 2;
+	} else if( countsHist[2] == 1 ) { // one pair
+		DEBUG_OUT( "Pair" );
+		result = 1;
+	}
+
+	return result << 20 | baseScore;
 }
--- poker.cpp
+++ poker.cpp
@@ -16,12 +16,92 @@
  */
 
 #include <cstdlib>
+#include <algorithm>
+#include <ctime>
 #include "card.hpp"
 
 using namespace std;
 
+vector<Card> generateDeck() {
+	vector<Card> deck;
+
+	for( int i = 0; i < 4; ++i ) {
+		for( int j = 0; j < 13; ++j ) {
+			deck.push_back( Card( static_cast<Suit>( i ), static_cast<Rank>( j ) ) );
+		}
+	}
+
+	return deck;
+}
+
+// find next k-combination
+void next_combination( uint64_t &x ) {
+	uint64_t u = x & -x;
+	uint64_t v = u + x;
+
+	x = v + ( ( ( v ^ x ) / u ) >> 2 );
+}
+
 int main() {
-	auto card1 = Card( Suit::diamonds, Rank::ace );
+	srand( unsigned( time( 0 ) ) );
+
+	auto deck = generateDeck();
+	//random_shuffle( deck.begin(), deck.end() );
+
+	//int i = 0;
+	//vector<Card> hand = { deck[i], deck[i + 1], deck[i + 2], deck[i + 3], deck[i + 4] };
+
+	vector<Card> hand;
+	hand.push_back( deck.back() );
+	deck.pop_back();
+	hand.push_back( deck.back() );
+	deck.pop_back();
+
+	//int deckScore = score( hand );
+
+	//cout << deckScore << endl;
+
+	//for( auto && card : hand ) {
+		//cout << card << endl;
+	//}
+
+	vector<Card> newHand = hand;
+
+	int n = 50; // number of cards remaining in deck
+	int k = 3; // number of cards to draw from deck
+
+	uint64_t x = ( 1 << k ) - 1;
+	uint8_t idx[k];
+	int best = 0;
+
+	while( x <= 4362862139015168 ) {
+		uint64_t m = x;
+		uint8_t *dst = idx;
+
+		for( int i = 0; i < n; ++i ) {
+			*dst = i;
+			dst += m & 1;
+			m >>= 1;
+		}
+
+		for( int i = 0; i < k; ++i ) {
+			newHand.push_back( deck[idx[i]] );
+		}
+
+		int deckScore = score( newHand );
+		
+		if( deckScore > best ) {
+			for( auto && card : newHand ) {
+				cout << card << ", ";
+			}
+
+			best = deckScore;
+			cout << deckScore << endl;
+		}
+		next_combination( x );
+		newHand = hand;
+	}
+
 	return 0;
 }
 
